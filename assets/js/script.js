c0c3de88f7cdf839a90b29686463c19ce471fe5f
FranciscoBarreira <Francisco.barreira1992@gmail.com>
2022-01-15 15:51:10 +0000

    add checkanswer

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType); 
@@ -39,15 +39,26 @@ function runGame(gameType) {
  
 
 
-function checkAnswer() {}
+function checkAnswer() {
+
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+    
+    if (isCorrect) {
+        alert("Hey! You Got it Right!");
+    } else {
+        alert(`Ups... You Answered ${userAnswer},The answer was ${calculatedAnswer[0]}!`)
+    }
+}
 
 function calculateCorrectAnswer() {
    let operand1 = parseInt(document.getElementById("operand1").innerText);
    let operand2 = parseInt(document.getElementById("operand2").innerText);
-   let operator = document.getElementById("operand1").innerText;
+   let operator = document.getElementById("operator").innerText;
 
    if (operator === "+") {
-       return (operand1 + operand2, "addition");
+       return [operand1 + operand2, "addition"];
    } else {
        alert(`Unimplemented Operator: ${operator} `);
        throw `Unimplemented Operator: ${operator}. Aborting!`;
