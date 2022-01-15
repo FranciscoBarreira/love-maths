fe433ea9d087cf2f3f9cc51f9fed76c27bffb62e
FranciscoBarreira <Francisco.barreira1992@gmail.com>
2022-01-15 16:28:55 +0000

    add subtract

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,6 +33,8 @@ document.addEventListener("DOMContentLoaded", function() {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
             displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract") {
+            displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -72,6 +74,8 @@ function calculateCorrectAnswer() {
        return [operand1 + operand2, "addition"];
    } else if(operator === "x") {
        return [operand1 * operand2, "multiply"];
+   } else if(operator === "-") {
+    return [operand1 - operand2, "subtract"];
    } else {
        alert(`Unimplemented Operator: ${operator} `);
        throw `Unimplemented Operator: ${operator}. Aborting!`;
@@ -97,7 +101,11 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById("operator").textContent = "+";
 }
 
-function displaySubstractQuestion () {}
+function displaySubtractQuestion (operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
+}
 
 function displayMultiplyQuestion (operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
