4338c6c11a796fd82a019d0a2853c8c4fb9f9eb4
FranciscoBarreira <Francisco.barreira1992@gmail.com>
2022-01-15 16:17:08 +0000

    add multiply

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+            displayMultiplyQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -68,6 +70,8 @@ function calculateCorrectAnswer() {
 
    if (operator === "+") {
        return [operand1 + operand2, "addition"];
+   } else if(operator === "x") {
+       return [operand1 * operand2, "multiply"];
    } else {
        alert(`Unimplemented Operator: ${operator} `);
        throw `Unimplemented Operator: ${operator}. Aborting!`;
@@ -95,5 +99,9 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubstractQuestion () {}
 
-function displayMultiplyQuestion () {}
+function displayMultiplyQuestion (operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
+}
 
